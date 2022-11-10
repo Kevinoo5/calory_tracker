608c07be2b783d80271f3ceccaf7116bf73ef348
Kevinoo5 <kevin.noormets@voco.ee>
2022-11-10 09:16:22 +0200

    Created Controllers and Data Structure

---
+++ app.js
@@ -0,0 +1,40 @@
+// Item Controller
+const itemCtrl = (function () {
+    const Item = function (id, name, calories){
+        this.id = id
+        this.name = name
+        this.calories = calories
+    }
+
+    const data = {
+        items: [
+            {id: 0, name: 'Steak Dinner', calories: 1200},
+            {id: 1, name: 'Cookie', calories: 400},
+            {id: 2, name: 'Eggs', calories: 300}
+            ],
+        total: 0
+    }
+
+    return {
+        logData: function (){
+            return data
+        }
+    }
+
+})();
+
+// UI Controller
+const UICtrl = (function () {
+    console.log("UICtrl")
+})();
+
+const App = (function (itemCtrl, UICtrl){
+    return {
+        init: function (){
+            console.log("Initializing App")
+        }
+    }
+})(itemCtrl, UICtrl)
+
+// Initialize App
+App.init()
